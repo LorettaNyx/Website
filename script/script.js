42973d69fa2c9c5dd7293e1a36c86d32dfbb31c2
LorettaNyx <LorettaNyx@users.noreply.github.com>
2017-02-05 15:52:08 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -6,10 +6,3 @@ function myFunction() {
         x.className = "nav-container";
     }
 }
-
-//paste this code under the head tag or in a separate js file.
-	// Wait for window load
-	$(window).load(function() {
-		// Animate loader off screen
-		$(".se-pre-con").fadeOut("slow");;
-	});
